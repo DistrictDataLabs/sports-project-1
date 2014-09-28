8ace0e9bf23c87ada93114a55d2c61203132370a
Matt Fifer <msfifer@gmail.com>
2014-09-28 16:33:19 -0400

    script ready to run

--- crawlScript_v2.py
+++ crawlScript_v2.py
@@ -2,9 +2,11 @@ import urllib2
 import os
 from bs4 import BeautifulSoup # donwload here: http://www.crummy.com/software/BeautifulSoup/#Download
 from datetime import datetime
+import re
+import json
 
 # this is a good utility function for getting text between two tags
-def getContents(htmlText, startTag, endTag, start=0, end=-1):
+def getContents(htmlText, startTag, endTag, start=0, end=-1, brkTag=[]):
     if end == -1:
         end = len(htmlText)
     startIndex = htmlText.find(startTag, start, end) + len(startTag)
@@ -12,7 +14,10 @@ def getContents(htmlText, startTag, endTag, start=0, end=-1):
     if startIndex == -1 or endIndex == -1:
         return ""
     else:
-        return htmlText[startIndex:endIndex]
+        if not brkTag or (brkTag and htmlText.find(brkTag, startIndex, endIndex) == -1):
+            return htmlText[startIndex:endIndex]
+        else:
+            return ""
 
 def parsePlayer(linkText): # link text of the form <a href = ...>Player Name</a>
     playerName = getContents(linkText, ">", "</a")
@@ -25,173 +30,233 @@ def parseStat(statText):
         return 0
     else:
         return int(statText)
+        
+def parseSub(subText):
+    if not subText:
+        return ()
+    minutes = int(re.findall(r"[0-9]+", getContents(subText, "<strong>Substitution</strong> - ", "\'"))[0])
+    subfor = getContents(subText, "Off: ", "');")
+    return (minutes, subfor)
 
 # os.path.join
 dropboxPath = "C:\Users\Matt\Documents\Dropbox\Sports Project\CrawlData"
 
-page_id = 200007;
-page_id_string = str(page_id).zfill(6)
+for page_id in [000004, 200007, 300001]:
 
-urlString = "http://www.espnfc.co.uk/gamecast/statistics/id/" + page_id_string + "/statistics.html"
-response = urllib2.urlopen(urlString)
-page_source = response.read()
-soup = BeautifulSoup(page_source)
-
-# get team names
-awayTeam = getContents(page_source, '<h1 id="away-team" class="heading alt">', '</h1>')
-homeTeam = getContents(page_source, '<h1 id="home-team" class="heading alt">', '</h1>')
-
-if not homeTeam or not awayTeam:
-    print page_id_string + ": Not a match!"
-else:
-    print awayTeam+' @ '+homeTeam
+    print "********************"
     
-# match date
-matchDate = getContents(page_source, "= new Date(", ")")
-matchDate = datetime.utcfromtimestamp(int(matchDate) / 1000)
-matchDateStr = matchDate.strftime("%Y-%m-%d, %H:%M")
-print matchDateStr
+    page_id_string = str(page_id).zfill(6)
+    print page_id_string
     
-# get stat column titles
-statAbbrevs = []
-statTitles = []
-for tag in soup.find_all("tr"):
-    if tag.th and tag.prettify().find("pstat") > -1:
-        statList = tag.find_all('th')
-        for stat in statList:
-            statAbbrevs.append(stat.text.strip())
-            if stat.has_attr("title"):
-                statTitles.append(stat.attrs["title"])
-            else:
-                statTitles.append(statAbbrevs[-1])
-        break
-
-# get the indices to search from
-awayIndex = page_source.find('<h1 id="away-team" class="heading alt">')
-homeIndex = page_source.find('<h1 id="home-team" class="heading alt">')
-
-# get the away lineup and subs
-awayLineup = getContents(page_source, '<td class="first">', 'Substitutes', start=awayIndex)
-awaySubs = getContents(page_source, 'Substitutes', '</table>', start=awayIndex)
-
-# get the home lineup and subs
-homeLineup = getContents(page_source, '<td class="first">', 'Substitutes', start=homeIndex)
-homeSubs = getContents(page_source, 'Substitutes', '</table>', start=homeIndex)
-
-# get away team starters!
-awayStarters = []
-startIndex=0
-while awayLineup.find('<p>', startIndex) > -1:
-    awayStarters.append(dict())
+    urlString = "http://www.espnfc.co.uk/gamecast/statistics/id/" + page_id_string + "/statistics.html"
+    response = urllib2.urlopen(urlString)
+    page_source = response.read()
+    soup = BeautifulSoup(page_source)
     
-    startTag = "<p>"    
-    endTag = "</p>"
-    awayStarters[-1][statAbbrevs[0]] = getContents(awayLineup, startTag, endTag, start=startIndex)
-    startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+    # get team names
+    awayTeam = getContents(page_source, '<h1 id="away-team" class="heading alt">', '</h1>')
+    homeTeam = getContents(page_source, '<h1 id="home-team" class="heading alt">', '</h1>')
+    leagueName = getContents(page_source, '<p class="floatleft">', "<span>").strip() # no clue why this is the only match
     
-    startTag = "<td>"
-    endTag = "</td>"
-    awayStarters[-1][statAbbrevs[1]] = int(getContents(awayLineup, startTag, endTag, start=startIndex))
-    startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+    if not soup.title.string.find(awayTeam + " v " + homeTeam) > -1 and not soup.title.string.find(homeTeam + " v " + awayTeam) > -1:
+        print "Not a match!"
+        continue
+    else:
+        print awayTeam + ' @ '+homeTeam + ", " + leagueName
+        
+    # match date
+    matchDate = getContents(page_source, "= new Date(", ")")
+    matchDate = datetime.utcfromtimestamp(int(matchDate) / 1000)
+    matchDateStr = matchDate.strftime("%Y-%m-%d, %H:%M")
+    print matchDateStr
+        
+    # get stat column titles, this is the only place that I use the BeautifulSoup object
+    statAbbrevs = []
+    statTitles = dict()
+    for tag in soup.find_all("tr"):
+        if tag.th and tag.prettify().find("pstat") > -1:
+            statList = tag.find_all('th')
+            for stat in statList:
+                statAbbrevs.append(stat.text.strip())
+                if stat.has_attr("title"):
+                    statTitles[statAbbrevs[-1]] = stat.attrs["title"]
+                else:
+                    statTitles[statAbbrevs[-1]] = statAbbrevs[-1]
+            break
+    statTitles["Sub"] = "Substitution (Min, Name)" # substitution labels
+    statTitles["Started"] = "Started" # started bool
     
-    startTag = "<td>"    
-    endTag = "</td>"
-    awayStarters[-1][statAbbrevs[2]] = parsePlayer(getContents(awayLineup, startTag, endTag, start=startIndex))
-    startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+    # get the indices to search from
+    awayIndex = page_source.find('<h1 id="away-team" class="heading alt">')
+    homeIndex = page_source.find('<h1 id="home-team" class="heading alt">')
     
-    for statLoop in range(3, len(statAbbrevs)):
-        startTag = "<td>"        
-        endTag = "</td>"
-        awayStarters[-1][statAbbrevs[statLoop]] = parseStat(getContents(awayLineup, startTag, endTag, start=startIndex))
+    # get the away lineup and subs
+    awayLineup = getContents(page_source, '<td class="first">', 'Substitutes', start=awayIndex)
+    awaySubLineup = getContents(page_source, 'Substitutes', '</table>', start=awayIndex)
+    
+    # get the home lineup and subs
+    homeLineup = getContents(page_source, '<td class="first">', 'Substitutes', start=homeIndex)
+    homeSubLineup = getContents(page_source, 'Substitutes', '</table>', start=homeIndex)
+    
+    # get away team starters!
+    awayStarters = []
+    startIndex=0
+    while awayLineup.find('<p>', startIndex) > -1:
+        awayStarters.append(dict())
+        awayStarters[-1]["Started"] = True
+        
+        # parse position
+        startTag = "<p>"    
+        endTag = "</p>"
+        awayStarters[-1][statAbbrevs[0]] = getContents(awayLineup, startTag, endTag, start=startIndex)
         startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
         
-    print awayStarters[-1]
-    
-# get home team starters!
-homeStarters = []
-startIndex=0
-while homeLineup.find('<p>', startIndex) > -1:
-    homeStarters.append(dict())
-    
-    startTag = "<p>"    
-    endTag = "</p>"
-    homeStarters[-1][statAbbrevs[0]] = getContents(homeLineup, startTag, endTag, start=startIndex)
-    startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    startTag = "<td>"
-    endTag = "</td>"
-    homeStarters[-1][statAbbrevs[1]] = int(getContents(homeLineup, startTag, endTag, start=startIndex))
-    startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    startTag = "<td>"    
-    endTag = "</td>"
-    homeStarters[-1][statAbbrevs[2]] = parsePlayer(getContents(homeLineup, startTag, endTag, start=startIndex))
-    startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    for statLoop in range(3, len(statAbbrevs)):
-        startTag = "<td>"        
+        # parse number
+        startTag = "<td>"
         endTag = "</td>"
-        homeStarters[-1][statAbbrevs[statLoop]] = parseStat(getContents(homeLineup, startTag, endTag, start=startIndex))
-        startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-        
-    print homeStarters[-1]
-
-# get away team subs!
-awaySubs = []
-startIndex=0
-while awayLineup.find('<p>', startIndex) > -1:
-    awaySubs.append(dict())
-    
-    startTag = "<p>"    
-    endTag = "</p>"
-    awaySubs[-1][statAbbrevs[0]] = getContents(awayLineup, startTag, endTag, start=startIndex)
-    startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    startTag = "<td>"
-    endTag = "</td>"
-    awaySubs[-1][statAbbrevs[1]] = int(getContents(awayLineup, startTag, endTag, start=startIndex))
-    startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    startTag = "<td>"    
-    endTag = "</td>"
-    awaySubs[-1][statAbbrevs[2]] = parsePlayer(getContents(awayLineup, startTag, endTag, start=startIndex))
-    startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    for statLoop in range(3, len(statAbbrevs)):
-        startTag = "<td>"        
-        endTag = "</td>"
-        awaySubs[-1][statAbbrevs[statLoop]] = parseStat(getContents(awayLineup, startTag, endTag, start=startIndex))
+        awayStarters[-1][statAbbrevs[1]] = int(getContents(awayLineup, startTag, endTag, start=startIndex))
         startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
         
-    print awaySubs[-1]
-    
-# get home team subs!
-homeSubs = []
-startIndex=0
-while homeLineup.find('<p>', startIndex) > -1:
-    homeSubs.append(dict())
-    
-    startTag = "<p>"    
-    endTag = "</p>"
-    homeSubs[-1][statAbbrevs[0]] = getContents(homeLineup, startTag, endTag, start=startIndex)
-    startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    startTag = "<td>"
-    endTag = "</td>"
-    homeSubs[-1][statAbbrevs[1]] = int(getContents(homeLineup, startTag, endTag, start=startIndex))
-    startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    startTag = "<td>"    
-    endTag = "</td>"
-    homeSubs[-1][statAbbrevs[2]] = parsePlayer(getContents(homeLineup, startTag, endTag, start=startIndex))
-    startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
-    
-    for statLoop in range(3, len(statAbbrevs)):
-        startTag = "<td>"        
+        # parse player name + id (espn.fc.co.uk ID)
+        startTag = "<td>"    
         endTag = "</td>"
-        homeSubs[-1][statAbbrevs[statLoop]] = parseStat(getContents(homeLineup, startTag, endTag, start=startIndex))
+        awayStarters[-1][statAbbrevs[2]] = parsePlayer(getContents(awayLineup, startTag, endTag, start=startIndex))
+        startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+        # 
+        for statLoop in range(3, len(statAbbrevs)):
+            startTag = "<td>"        
+            endTag = "</td>"
+            awayStarters[-1][statAbbrevs[statLoop]] = parseStat(getContents(awayLineup, startTag, endTag, start=startIndex))
+            startIndex = awayLineup.find(endTag, awayLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+    # get home team starters!
+    homeStarters = []
+    startIndex=0
+    while homeLineup.find('<p>', startIndex) > -1:
+        homeStarters.append(dict())
+        homeStarters[-1]["Started"] = True
+        
+        startTag = "<p>"    
+        endTag = "</p>"
+        homeStarters[-1][statAbbrevs[0]] = getContents(homeLineup, startTag, endTag, start=startIndex)
         startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
         
-    print homeSubs[-1]
-
+        startTag = "<td>"
+        endTag = "</td>"
+        homeStarters[-1][statAbbrevs[1]] = int(getContents(homeLineup, startTag, endTag, start=startIndex))
+        startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
         
+        startTag = "<td>"    
+        endTag = "</td>"
+        homeStarters[-1][statAbbrevs[2]] = parsePlayer(getContents(homeLineup, startTag, endTag, start=startIndex))
+        startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+        for statLoop in range(3, len(statAbbrevs)):
+            startTag = "<td>"        
+            endTag = "</td>"
+            homeStarters[-1][statAbbrevs[statLoop]] = parseStat(getContents(homeLineup, startTag, endTag, start=startIndex))
+            startIndex = homeLineup.find(endTag, homeLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+    
+    # get away team subs!
+    awaySubs = []
+    startIndex=0
+    while awaySubLineup.find('<p>', startIndex) > -1:
+        awaySubs.append(dict())
+        awaySubs[-1]["Started"] = False
+        
+        startTag = "<p>"    
+        endTag = "</p>"
+        awaySubs[-1][statAbbrevs[0]] = getContents(awaySubLineup, startTag, endTag, start=startIndex)
+        startIndex = awaySubLineup.find(endTag, awaySubLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+        startTag = "<td>"
+        endTag = "</td>"
+        awaySubs[-1][statAbbrevs[1]] = int(getContents(awaySubLineup, startTag, endTag, start=startIndex))
+        startIndex = awaySubLineup.find(endTag, awaySubLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+        # parse player name and ID
+        startTag = "<td>"    
+        endTag = "</td>"
+        awaySubs[-1][statAbbrevs[2]] = parsePlayer(getContents(awaySubLineup, startTag, endTag, start=startIndex))
+        
+        # parse sub information if it exists    
+        startTag = '<div '
+        endTag = "</div>"
+        breakTag = "</td>"# this can be used to say "there is no content here
+        awaySubs[-1]["Sub"] = parseSub(getContents(awaySubLineup, startTag, endTag, start=startIndex, brkTag=breakTag))
+        startIndex = awaySubLineup.find(breakTag, startIndex) + len(breakTag)
+        
+        for statLoop in range(3, len(statAbbrevs)):
+            startTag = "<td>"        
+            endTag = "</td>"
+            awaySubs[-1][statAbbrevs[statLoop]] = parseStat(getContents(awaySubLineup, startTag, endTag, start=startIndex))
+            startIndex = awaySubLineup.find(endTag, awaySubLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+    # get home team subs!
+    homeSubs = []
+    startIndex=0
+    while homeSubLineup.find('<p>', startIndex) > -1:
+        homeSubs.append(dict())
+        homeSubs[-1]["Started"] = False
+        
+        startTag = "<p>"    
+        endTag = "</p>"
+        homeSubs[-1][statAbbrevs[0]] = getContents(homeSubLineup, startTag, endTag, start=startIndex)
+        startIndex = homeSubLineup.find(endTag, homeSubLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+        startTag = "<td>"
+        endTag = "</td>"
+        homeSubs[-1][statAbbrevs[1]] = int(getContents(homeSubLineup, startTag, endTag, start=startIndex))
+        startIndex = homeSubLineup.find(endTag, homeSubLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+        
+        startTag = "<td>"    
+        endTag = "</td>"
+        homeSubs[-1][statAbbrevs[2]] = parsePlayer(getContents(homeSubLineup, startTag, endTag, start=startIndex))
+            
+        # parse sub information if it exists    
+        startTag = '<div '
+        endTag = "</div>"
+        breakTag = "</td>"# this can be used to say "there is no content here
+        homeSubs[-1]["Sub"] = parseSub(getContents(homeSubLineup, startTag, endTag, start=startIndex, brkTag=breakTag))
+        startIndex = homeSubLineup.find(breakTag, startIndex) + len(breakTag)
+        
+        for statLoop in range(3, len(statAbbrevs)):
+            startTag = "<td>"        
+            endTag = "</td>"
+            homeSubs[-1][statAbbrevs[statLoop]] = parseStat(getContents(homeSubLineup, startTag, endTag, start=startIndex))
+            startIndex = homeSubLineup.find(endTag, homeSubLineup.find(startTag, startIndex) + len(startTag)) + len(endTag)
+    
+    # create the json object for writing
+    jsonData = dict()
+    jsonData["League"] = leagueName
+    jsonData["Home Team"] = homeTeam
+    jsonData["Away Team"] = awayTeam
+    jsonData["Game Date, Time"] = matchDateStr
+    jsonData["Stat Abbreviations"] = statTitles
+    jsonData["Home Players"] = homeStarters
+    jsonData["Home Players"].extend(homeSubs)
+    jsonData["Away Players"] = awayStarters
+    jsonData["Away Players"].extend(awaySubs)
+    jsonData["Away Score"] = sum([player["G"] for player in jsonData["Away Players"]])
+    jsonData["Home Score"] = sum([player["G"] for player in jsonData["Home Players"]])
+    
+    # write everything to a json
+    filename = matchDate.strftime("%Y-%m-%d_%H%M") + "_" + (awayTeam + " at " + homeTeam).replace(" ", "-") + ".json"
+    
+    # make sure the file structure exists
+    if not os.path.exists(os.path.join(dropboxPath, leagueName, str(matchDate.year))):
+        if not os.path.exists(os.path.join(dropboxPath, leagueName)):
+            os.mkdir(os.path.join(dropboxPath, leagueName))
+        os.mkdir(os.path.join(dropboxPath, leagueName, str(matchDate.year)))
+        
+    # write the json
+    with open(os.path.join(dropboxPath, leagueName, str(matchDate.year), filename), 'w') as outfile:
+        json.dump(jsonData, outfile)
+        print "Wrote JSON " + filename + " successfully!"
+        
+    csvEntry = page_id_string + "," + leagueName + "," + awayTeam + "," + homeTeam + "," + matchDate.strftime("'%Y-%m-%d %H:%M'") + ",'" + str(jsonData["Away Score"]) + "-" + str(jsonData["Home Score"]) + "'," + os.path.join(leagueName, str(matchDate.year), filename) + "\n"
+    with open(os.path.join(dropboxPath, "MatchesExtracted.csv"), 'a') as csvwriter:
+        csvwriter.write(csvEntry)
+        print "Wrote entry into MatchesExtracted.csv successfully!"    
+    
